582c27d7fff0cbe09d0cf00a7a55aa7731b5fdb5
codebender <khoa28122005@gmail.com>
2024-08-17 17:20:15 +0700

    Added Window Border For Cursor

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 public class DragAndDrop : MonoBehaviour
 {
     private bool _isDragging = false;
+    private Vector3 distanceFromPosToMousePos;
+    private Vector3 mousePos;
     public void GraperFunc()
     {
         if (!_isDragging) return;
         transform.position = GetFixedPos();
     }
     private void OnMouseDown()
-    => _isDragging = true;
+    {
+        _isDragging = true;
+        distanceFromPosToMousePos = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
     private void OnMouseUp()
     => _isDragging = false;
     private Vector3 GetFixedPos()
     {
-        Vector3 cameraPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 cameraPos = distanceFromPosToMousePos + Camera.main.ScreenToWorldPoint(Input.mousePosition);
         return new Vector3(cameraPos.x, cameraPos.y, transform.position.z);
     }
 }
--- Assets/Scripts/WindowResizer.cs
+++ Assets/Scripts/WindowResizer.cs
@@ -1,46 +1,142 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class WindowResizer : MonoBehaviour
 {
-    enum GraporSize { Size, Grap};
     private Vector3 originalMousePosition;
-    private Vector3 originalPosition;
     private Vector3 originalScale;
-    private float xScaleMultiplier;
-    private float yScaleMultiplier;
+    private Vector3 originalWindowPosition;
+    private bool resizing;
+
+    private enum ResizeDirection { None, Left, Right, Top, Bottom, TopLeft, TopRight, BottomLeft, BottomRight }
+    private ResizeDirection resizeDirection = ResizeDirection.None;
+
+    public BoxCollider2D topLeftCollider, topRightCollider, bottomLeftCollider, bottomRightCollider;
+    public BoxCollider2D leftCollider, rightCollider, topCollider, bottomCollider;
     public void SizerFunc()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            InitializeResizeWindow();
-        }
-        if (Input.GetMouseButton(0))
-        {
-            ResizeWindow();
-        }
-    }
-    private void InitializeResizeWindow()
-    {
-        originalMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        originalPosition = transform.position;
-        originalScale = transform.localScale;
-        Vector2 delta = originalMousePosition - originalPosition;
+            Vector3 mousePosition = GetMouseWorldPosition();
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
-        if (delta.x > 0 && delta.y > 0) { xScaleMultiplier = 1; yScaleMultiplier = 1; } //Cursor starts at top right corner
-        if (delta.x > 0 && delta.y < 0) { xScaleMultiplier = 1; yScaleMultiplier = -1; } //Cursor starts at bottom right corner
-        if (delta.x < 0 && delta.y > 0) { xScaleMultiplier = -1; yScaleMultiplier = 1; } //Cursor starts at top left corner
-        if (delta.x < 0 && delta.y < 0) { xScaleMultiplier = -1; yScaleMultiplier = -1; } //Cursor starts at bottom left corner
-    }
-    private void ResizeWindow()
-    {
-        Vector3 mousePositionDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - originalMousePosition;
-        transform.position = originalPosition + mousePositionDelta / 2;
-        if (mousePositionDelta.magnitude > 0.1)
-        {
-            transform.localScale = new Vector3(originalScale.x + mousePositionDelta.x * xScaleMultiplier, originalScale.y + mousePositionDelta.y * yScaleMultiplier, 1);
+            if (hit.collider != null)
+            {
+                if (hit.collider == topLeftCollider)
+                {
+                    resizeDirection = ResizeDirection.TopLeft;
+                }
+                else if (hit.collider == topRightCollider)
+                {
+                    resizeDirection = ResizeDirection.TopRight;
+                }
+                else if (hit.collider == bottomLeftCollider)
+                {
+                    resizeDirection = ResizeDirection.BottomLeft;
+                }
+                else if (hit.collider == bottomRightCollider)
+                {
+                    resizeDirection = ResizeDirection.BottomRight;
+                }
+                else if (hit.collider == leftCollider)
+                {
+                    resizeDirection = ResizeDirection.Left;
+                }
+                else if (hit.collider == rightCollider)
+                {
+                    resizeDirection = ResizeDirection.Right;
+                }
+                else if (hit.collider == topCollider)
+                {
+                    resizeDirection = ResizeDirection.Top;
+                }
+                else if (hit.collider == bottomCollider)
+                {
+                    resizeDirection = ResizeDirection.Bottom;
+                }
+            }
+
+            if (resizeDirection != ResizeDirection.None)
+            {
+                resizing = true;
+                originalMousePosition = mousePosition;
+                originalScale = transform.localScale;
+                originalWindowPosition = transform.position;
+            }
         }
+
+        if (Input.GetMouseButton(0) && resizing)
+        {
+            Vector3 currentMousePosition = GetMouseWorldPosition();
+            Vector3 delta = currentMousePosition - originalMousePosition;
+
+            ResizeWindow(delta);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            resizing = false;
+            resizeDirection = ResizeDirection.None;
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        mouseScreenPosition.z = Mathf.Abs(Camera.main.transform.position.z - transform.position.z);
+        return Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+    }
+
+    private void ResizeWindow(Vector3 delta)
+    {
+        Vector3 newScale = originalScale;
+        Vector3 newPosition = originalWindowPosition;
+
+        switch (resizeDirection)
+        {
+            case ResizeDirection.Left:
+                newScale.x = originalScale.x - delta.x;
+                newPosition.x = originalWindowPosition.x + delta.x / 2;
+                break;
+            case ResizeDirection.Right:
+                newScale.x = originalScale.x + delta.x;
+                newPosition.x = originalWindowPosition.x + delta.x / 2;
+                break;
+            case ResizeDirection.Top:
+                newScale.y = originalScale.y + delta.y;
+                newPosition.y = originalWindowPosition.y + delta.y / 2;
+                break;
+            case ResizeDirection.Bottom:
+                newScale.y = originalScale.y - delta.y;
+                newPosition.y = originalWindowPosition.y + delta.y / 2;
+                break;
+            case ResizeDirection.TopLeft:
+                newScale.x = originalScale.x - delta.x;
+                newScale.y = originalScale.y + delta.y;
+                newPosition.x = originalWindowPosition.x + delta.x / 2;
+                newPosition.y = originalWindowPosition.y + delta.y / 2;
+                break;
+            case ResizeDirection.TopRight:
+                newScale.x = originalScale.x + delta.x;
+                newScale.y = originalScale.y + delta.y;
+                newPosition.x = originalWindowPosition.x + delta.x / 2;
+                newPosition.y = originalWindowPosition.y + delta.y / 2;
+                break;
+            case ResizeDirection.BottomLeft:
+                newScale.x = originalScale.x - delta.x;
+                newScale.y = originalScale.y - delta.y;
+                newPosition.x = originalWindowPosition.x + delta.x / 2;
+                newPosition.y = originalWindowPosition.y + delta.y / 2;
+                break;
+            case ResizeDirection.BottomRight:
+                newScale.x = originalScale.x + delta.x;
+                newScale.y = originalScale.y - delta.y;
+                newPosition.x = originalWindowPosition.x + delta.x / 2;
+                newPosition.y = originalWindowPosition.y + delta.y / 2;
+                break;
+        }
+
+        transform.localScale = newScale;
+        transform.position = newPosition;
     }
 }
