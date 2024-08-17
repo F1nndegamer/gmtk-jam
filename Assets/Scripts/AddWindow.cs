6f1bfff72ec7ab5e524b3c8c614e34baab13d118
icantdo <80340838+icantdo@users.noreply.github.com>
2024-08-17 10:07:23 +0300

    Update AddWindow.cs

--- Assets/Scripts/AddWindow.cs
+++ Assets/Scripts/AddWindow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AddWindow : MonoBehaviour
 {
+    public GameObject MaskWindow;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class AddWindow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+            Instantiate(MaskWindow, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
     }
 }
