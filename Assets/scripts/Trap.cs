1168f3c2144c81909f2b9c077ce595659392b183
icantdo <80340838+icantdo@users.noreply.github.com>
2024-08-17 18:55:50 +0300

    Import Player Movement and Trap

--- Assets/scripts/Trap.cs
+++ Assets/scripts/Trap.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Trap : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
-            Debug.Log("you die hahahahahah");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
