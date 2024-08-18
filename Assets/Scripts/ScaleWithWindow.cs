c268ca3c56916c98cadf7a08f75f291f260d120e
codebender <khoa28122005@gmail.com>
2024-08-19 01:17:32 +0700

    Added Independant Mode For Scalable Objects

--- Assets/Scripts/ScaleWithWindow.cs
+++ Assets/Scripts/ScaleWithWindow.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ScaleWithWindow : MonoBehaviour
 {
+    private enum ScaleMode { Uniform, Independant }
     [SerializeField] private GameObject MaskWindow;
     [SerializeField] float scaleSpeed = 0.5f;
+    [SerializeField] ScaleMode scaleMode = ScaleMode.Uniform;
     private Vector3 initialScale;
     private Vector3 initialMaskScale;
 
@@ -20,8 +22,17 @@ public class ScaleWithWindow : MonoBehaviour
         Vector3 scaleDifference = MaskWindow.transform.localScale - initialMaskScale;
         Vector3 scaledDifference = scaleDifference * scaleSpeed;
         Vector3 newScale = initialScale + scaledDifference;
-        float uniformScale = Mathf.Max(newScale.x, newScale.y);
-        transform.localScale = new Vector3(uniformScale, uniformScale, transform.localScale.z);
+        switch (scaleMode)
+        {
+            case ScaleMode.Uniform:
+                float uniformScale = Mathf.Max(newScale.x, newScale.y);
+                transform.localScale = new Vector3(uniformScale, uniformScale, transform.localScale.z);
+                break;
+            case ScaleMode.Independant:
+                transform.localScale = new Vector3(newScale.x, newScale.y, transform.localScale.z);
+                break;
+        }
+
         if (transform.localScale.x < 0.2f && transform.localScale.y < 0.2f)
         {
             Destroy(gameObject);
